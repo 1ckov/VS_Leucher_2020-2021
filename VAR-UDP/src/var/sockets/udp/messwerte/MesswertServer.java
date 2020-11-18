ba4503fdfca05ac74848bf83d867f466fc51ed6b
Ickov96 <sa6o.hristov96@gmail.com>
2020-11-18 07:18:13 +0100

    Finished Book Assignments UDP except the bonus one

--- VAR-UDP/src/var/sockets/udp/messwerte/MesswertServer.java
+++ VAR-UDP/src/var/sockets/udp/messwerte/MesswertServer.java
@@ -1,8 +1,26 @@
 package var.sockets.udp.messwerte;
 
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+
+
 public class MesswertServer {
+  private static final int PORT = 4117;
+  private static final int BUFSEIZE = 512;
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    try (DatagramSocket socket = new DatagramSocket(PORT)) {
+      DatagramPacket packetIn = new DatagramPacket(new byte[BUFSEIZE], BUFSEIZE);
+
+      System.out.println("Server started...");
+      while(true){
+        socket.receive(packetIn);
+        String receivedData = new String(packetIn.getData(), 0, packetIn.getLength());
+        String senderAddr = packetIn.getAddress().getHostAddress() + ":" + packetIn.getPort();
+        System.out.println(senderAddr + " " + receivedData);
+      }
+    } catch (Exception e) {
+      System.err.println(e);
+    }
   }
 }
